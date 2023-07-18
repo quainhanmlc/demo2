4f900e0765d633161c8abe99533b08804ff97e80
quainhanmlc <quainhanmlc@gmail.com>
2023-07-18 11:49:18 +0700

    update 3

--- web4.js
+++ web4.js
@@ -83,6 +83,4 @@ const server = http.createServer((req, res)=> {
         res.end()
    }
 });
-server.listen(port, () => {
-    console.log("Server is running at http://localhost:" +port);
-})
+server.listen(port);
